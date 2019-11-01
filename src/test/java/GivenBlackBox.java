5978cffe712f5a1791d267f4c7a83b8412b7ed10
Barney Goette <bgoette@asu.edu>
2019-11-01 10:57:55 -0500

    updated formatting

--- src/test/java/GivenBlackBox.java
+++ src/test/java/GivenBlackBox.java
@@ -17,29 +17,29 @@ import main.java.Course;
 import static org.junit.Assert.*;
 
 @RunWith(Parameterized.class)
-public class GivenBlackBox {
+public class GivenBlackBox
+{
     private Class<Course> classUnderTest;
 
     @SuppressWarnings("unchecked")
-    public GivenBlackBox(Object classUnderTest) {
+    public GivenBlackBox(Object classUnderTest)
+    {
         this.classUnderTest = (Class<Course>) classUnderTest;
     }
 
     @Parameters
-    public static Collection<Object[]> courseGradesUnderTest() {
-        Object[][] classes = {
-                {CourseGrades0.class},
-                {CourseGrades1.class},
-                {CourseGrades2.class},
-                {CourseGrades3.class},
-                {CourseGrades4.class},
-                {CourseGrades5.class}
+    public static Collection<Object[]> courseGradesUnderTest()
+    {
+        Object[][] classes = { { CourseGrades0.class }, { CourseGrades1.class }, { CourseGrades2.class },
+                { CourseGrades3.class }, { CourseGrades4.class }, { CourseGrades5.class }
 
         };
+        
         return Arrays.asList(classes);
     }
 
-    private Course createCourse(String name) throws Exception {
+    private Course createCourse(String name) throws Exception
+    {
         Constructor<Course> constructor = classUnderTest.getConstructor(String.class);
         return constructor.newInstance(name);
     }
@@ -50,59 +50,61 @@ public class GivenBlackBox {
     Course happyDayGradeBoundary;
     HashMap<String, String> happyDayGradeBoundaryExpected = new HashMap<String, String>();
 
-
     @Before
-    public void setUp() throws Exception {
+    public void setUp() throws Exception
+    {
 
         // One Student
 
-
         // all courses should be created like this
-
 
         // Course created with two Students having
         oneStudent = createCourse("SER316");
 
-        // this would be the expected result after the method countOccurencesLetterGrades is called
+        // this would be the expected result after the method
+        // countOccurencesLetterGrades is called
         oneStudent.set_points("Hanna", 50);
         oneStudentExpected.put("Hanna", "A");
 
         // Happy Day Case Grade Boundaries
         // Four Students mix of grades
         happyDayGradeBoundary = createCourse("SER315");
-        happyDayGradeBoundary.set_points("100"  , 100);  //A
-        happyDayGradeBoundary.set_points(">89"  , 90);  //A
-        happyDayGradeBoundary.set_points(">79"  , 89);  //B
-        happyDayGradeBoundary.set_points(">59"  , 79);  //C
-        happyDayGradeBoundary.set_points(">35"  , 59);  //D
-        happyDayGradeBoundary.set_points(">=35" , 35);  //F
+        happyDayGradeBoundary.set_points("100", 100); // A
+        happyDayGradeBoundary.set_points(">89", 90); // A
+        happyDayGradeBoundary.set_points(">79", 89); // B
+        happyDayGradeBoundary.set_points(">59", 79); // C
+        happyDayGradeBoundary.set_points(">35", 59); // D
+        happyDayGradeBoundary.set_points(">=35", 35); // F
 
-        happyDayGradeBoundaryExpected.put("100"  , "A");
-        happyDayGradeBoundaryExpected.put(">89"  , "A");
-        happyDayGradeBoundaryExpected.put(">79"  , "B");
-        happyDayGradeBoundaryExpected.put(">59"  , "C");
-        happyDayGradeBoundaryExpected.put(">35"  , "D");
-        happyDayGradeBoundaryExpected.put(">=35" , "F");
+        happyDayGradeBoundaryExpected.put("100", "A");
+        happyDayGradeBoundaryExpected.put(">89", "A");
+        happyDayGradeBoundaryExpected.put(">79", "B");
+        happyDayGradeBoundaryExpected.put(">59", "C");
+        happyDayGradeBoundaryExpected.put(">35", "D");
+        happyDayGradeBoundaryExpected.put(">=35", "F");
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() throws Exception
+    {
     }
 
     // sample test
     @Test
-    public void oneStudent() {
+    public void oneStudent()
+    {
         Map<String, String> ans = oneStudent.curveLetterGrades();
-        for(Map.Entry<String, String> e : ans.entrySet())
+        for (Map.Entry<String, String> e : ans.entrySet())
             System.out.println(e.getKey() + " " + e.getValue());
         assertTrue(ans.equals(oneStudentExpected));
     }
 
     // sample test2
     @Test
-    public void happyDayGradeBoundaries() {
+    public void happyDayGradeBoundaries()
+    {
         Map<String, String> ans = happyDayGradeBoundary.curveLetterGrades();
-        for(Map.Entry<String, String> e : ans.entrySet())
+        for (Map.Entry<String, String> e : ans.entrySet())
             System.out.println(e.getKey() + " " + e.getValue());
         assertTrue(ans.equals(happyDayGradeBoundaryExpected));
     }
