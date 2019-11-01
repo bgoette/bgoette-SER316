e03b2fe9cd9b97dd45661a27079c1c8c066850ce
Barney Goette <bgoette@asu.edu>
2019-11-01 15:01:59 -0500

    Implemented upper/lower partition testing

--- src/test/java/GivenBlackBox.java
+++ src/test/java/GivenBlackBox.java
@@ -30,8 +30,13 @@ public class GivenBlackBox
     @Parameters
     public static Collection<Object[]> courseGradesUnderTest()
     {
-        Object[][] classes = { { CourseGrades0.class }, { CourseGrades1.class }, { CourseGrades2.class },
-                { CourseGrades3.class }, { CourseGrades4.class }, { CourseGrades5.class }
+        Object[][] classes = { 
+                { CourseGrades0.class }, 
+                { CourseGrades1.class }, 
+                { CourseGrades2.class },
+                { CourseGrades3.class }, 
+                { CourseGrades4.class }, 
+                { CourseGrades5.class }
 
         };
         
@@ -50,10 +55,15 @@ public class GivenBlackBox
     Course happyDayGradeBoundary;
     HashMap<String, String> happyDayGradeBoundaryExpected = new HashMap<String, String>();
 
+    Course lowerPartition;
+    HashMap<String, String> lowerPartitionExpected = new HashMap<String, String>();
+
+    Course upperPartition;
+    HashMap<String, String> upperPartitionExpected = new HashMap<String, String>();
+
     @Before
     public void setUp() throws Exception
     {
-
         // One Student
 
         // all courses should be created like this
@@ -74,14 +84,30 @@ public class GivenBlackBox
         happyDayGradeBoundary.set_points(">79", 89); // B
         happyDayGradeBoundary.set_points(">59", 79); // C
         happyDayGradeBoundary.set_points(">35", 59); // D
-        happyDayGradeBoundary.set_points(">=35", 35); // F
+        happyDayGradeBoundary.set_points("<=35", 35); // F
 
         happyDayGradeBoundaryExpected.put("100", "A");
         happyDayGradeBoundaryExpected.put(">89", "A");
         happyDayGradeBoundaryExpected.put(">79", "B");
         happyDayGradeBoundaryExpected.put(">59", "C");
         happyDayGradeBoundaryExpected.put(">35", "D");
-        happyDayGradeBoundaryExpected.put(">=35", "F");
+        happyDayGradeBoundaryExpected.put("<=35", "F");
+        
+        lowerPartition = createCourse("Lower");
+        
+        lowerPartition.set_points("<0", -1);
+        lowerPartition.set_points(">=0", 100);
+        
+        lowerPartitionExpected.put("<0", "F");
+        lowerPartitionExpected.put(">=0", "A");
+        
+        upperPartition = createCourse("Upper");
+        
+        upperPartition.set_points(">100", 110);
+        upperPartition.set_points("=35", 35);
+        
+        upperPartitionExpected.put(">100", "A");
+        upperPartitionExpected.put("=35", "F");
     }
 
     @After
@@ -109,4 +135,21 @@ public class GivenBlackBox
         assertTrue(ans.equals(happyDayGradeBoundaryExpected));
     }
 
+    @Test
+    public void lowerPartitionBoundary()
+    {
+        Map<String, String> ans = lowerPartition.curveLetterGrades();
+        for (Map.Entry<String, String> e : ans.entrySet())
+            System.out.println("Lower: " + e.getKey() + " " + e.getValue());
+        assertTrue(ans.equals(lowerPartitionExpected));
+    }
+
+    @Test
+    public void upperPartitionBoundary()
+    {
+        Map<String, String> ans = upperPartition.curveLetterGrades();
+        for (Map.Entry<String, String> e : ans.entrySet())
+            System.out.println("Upper: " + e.getKey() + " " + e.getValue());
+        assertTrue(ans.equals(upperPartitionExpected));
+    }
 }
