e40794936fc59ae4d95ddc0f337dae3979ebe465
bgoette <bgoette@asu.edu>
2019-11-11 19:05:53 -0600

    added unit tests for each test sequence in flow graph

--- src/test/java/CourseTest.java
+++ src/test/java/CourseTest.java
@@ -21,19 +21,151 @@ public class CourseTest
     @Test
     public void noCurveOneF()
     {
-        Course test1;
-        HashMap<String, Integer> test1Expected = new HashMap<String, Integer>();
+        Course course;
+        HashMap<String, Integer> courseExpected = new HashMap<String, Integer>();
         
-        test1 = new Course("SER316");
-        test1.set_points("=35", 35);
+        course = new Course("SER316");
+        course.set_points("F", 35);
         
-        test1Expected.put("A", 0);
-        test1Expected.put("B", 0);
-        test1Expected.put("C", 0);
-        test1Expected.put("D", 0);
-        test1Expected.put("F", 1);
+        courseExpected.put("A", 0);
+        courseExpected.put("B", 0);
+        courseExpected.put("C", 0);
+        courseExpected.put("D", 0);
+        courseExpected.put("F", 1);
         
-        HashMap<String, Integer> ans = test1.countOccurencesLetterGrades(false);
-        assertTrue(ans.equals(test1Expected));
+        HashMap<String, Integer> ans = course.countOccurencesLetterGrades(false);
+        assertTrue(ans.equals(courseExpected));
+    }
+
+    /*
+     * Test Case: { < 197,198,199,200,201,202,203,204,205,206,210,211,213,215,217,218,210,227 > }
+     */
+    @Test
+    public void noCurveOneD()
+    {
+        Course course;
+        HashMap<String, Integer> courseExpected = new HashMap<String, Integer>();
+        
+        course = new Course("SER316");
+        course.set_points("D", 36);
+        
+        courseExpected.put("A", 0);
+        courseExpected.put("B", 0);
+        courseExpected.put("C", 0);
+        courseExpected.put("D", 1);
+        courseExpected.put("F", 0);
+        
+        HashMap<String, Integer> ans = course.countOccurencesLetterGrades(false);
+        assertTrue(ans.equals(courseExpected));
+    }
+    
+    /*
+     * Test Case: { < 197,198,199,200,201,202,203,204,205,206,210,211,213,215,216,210,227 > }
+     */
+    @Test
+    public void noCurveOneC()
+    {
+        Course course;
+        HashMap<String, Integer> courseExpected = new HashMap<String, Integer>();
+        
+        course = new Course("SER316");
+        course.set_points("C", 60);
+        
+        courseExpected.put("A", 0);
+        courseExpected.put("B", 0);
+        courseExpected.put("C", 1);
+        courseExpected.put("D", 0);
+        courseExpected.put("F", 0);
+        
+        HashMap<String, Integer> ans = course.countOccurencesLetterGrades(false);
+        assertTrue(ans.equals(courseExpected));
+    }
+    
+    /*
+     * Test Case: { < 197,198,199,200,201,202,203,204,205,206,210,211,213,214,210,227 > }
+     */
+    @Test
+    public void noCurveOneB()
+    {
+        Course course;
+        HashMap<String, Integer> courseExpected = new HashMap<String, Integer>();
+        
+        course = new Course("SER316");
+        course.set_points("B", 85);
+        
+        courseExpected.put("A", 0);
+        courseExpected.put("B", 1);
+        courseExpected.put("C", 0);
+        courseExpected.put("D", 0);
+        courseExpected.put("F", 0);
+        
+        HashMap<String, Integer> ans = course.countOccurencesLetterGrades(false);
+        assertTrue(ans.equals(courseExpected));
+    }
+    
+    /*
+     * Test Case: { < 197,198,199,200,201,202,203,204,205,206,210,211,212,210,227 > }
+     */
+    @Test
+    public void noCurveOneA()
+    {
+        Course course;
+        HashMap<String, Integer> courseExpected = new HashMap<String, Integer>();
+        
+        course = new Course("SER316");
+        course.set_points("A", 100);
+        
+        courseExpected.put("A", 1);
+        courseExpected.put("B", 0);
+        courseExpected.put("C", 0);
+        courseExpected.put("D", 0);
+        courseExpected.put("F", 0);
+        
+        HashMap<String, Integer> ans = course.countOccurencesLetterGrades(false);
+        assertTrue(ans.equals(courseExpected));
+    }
+    
+    /*
+     * Test Case: { < 197,198,199,200,201,202,203,204,205,206,207 > }
+     */
+    @Test
+    public void nullPointerTest()
+    {
+        Course course;
+        
+        course = new Course("SER316");
+        
+        try
+        {
+            HashMap<String, Integer> ans = course.countOccurencesLetterGrades(false);
+            
+            assertTrue(false);
+        }
+        catch (NullPointerException e)
+        {
+            assertNotNull(e);
+        }
+    }
+    
+    /*
+     * Test Case: { < 197,198,199,200,201,202,203,204,223,224,225,224,227 > }
+     */
+    @Test
+    public void curveOneA()
+    {
+        Course course;
+        HashMap<String, Integer> courseExpected = new HashMap<String, Integer>();
+        
+        course = new Course("SER316");
+        course.set_points("A", 100);
+        
+        courseExpected.put("A", 1);
+        courseExpected.put("B", 0);
+        courseExpected.put("C", 0);
+        courseExpected.put("D", 0);
+        courseExpected.put("F", 0);
+        
+        HashMap<String, Integer> ans = course.countOccurencesLetterGrades(true);
+        assertTrue(ans.equals(courseExpected));
     }
 }
